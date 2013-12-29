1a4ffb8d9996cb9344b3ad11e66c2c9aedc49346
Sebastian Geiger <sebastian.geiger@gmail.com>
2013-12-29 21:17:31 +0100

    Configured vcr

--- spec/acceptance/searching_ebookers_for_fra_sfo_spec.rb
+++ spec/acceptance/searching_ebookers_for_fra_sfo_spec.rb
@@ -1,8 +1,7 @@
 require_relative '../../lib/flight_scraper'
-require 'pry'
-require 'launchy'
+require_relative '../spec_helper'
 
-describe 'Searching for a flight from FRA to SFO yields some results' do
+describe 'Searching for a flight from FRA to SFO yields some results', :vcr do
   it 'returns 45 results' do
     segment = FlightScraper::Segment.new("FRA", "SFO", Date.new(2014,2,15))
     results = FlightScraper::Search.new(segment).execute
